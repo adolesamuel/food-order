ab9671096e17056e164bdc5be633b6a695189b8d
Adole Samuel Sani <adolesamuel4@gmail.com>
2025-08-13 14:01:53 +0100

    cart item as modal done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,20 @@ import { useContext } from 'react';
 import logoImg from '../assets/logo.jpg';
 import Button from './UI/Button';
 import CartContext from '../store/CartContext';
+import UserProgressContext from '../store/UserProgressContext';
 
 export default function Header() {
 
     const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
 
     const count = cartCtx.items.reduce((totalNumberOfItems, item) => {
         return totalNumberOfItems + item.quantity
     }, 0);
+
+    function handleShowCart() {
+        userProgressCtx.showCart();
+    }
 
     return <header id="main-header">
         <div id="title">
@@ -18,7 +24,7 @@ export default function Header() {
             <h1>React Food</h1>
         </div>
         <nav>
-            <Button textOnly>
+            <Button textOnly onClick={handleShowCart}>
                 Cart({count})
             </Button>
         </nav>
